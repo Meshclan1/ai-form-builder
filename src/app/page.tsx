98e9ce528db1b86efa8ce5a1bfb7268c14cb6c7c
Ayo <Ayo.meshe@gmail.com>
2024-04-03 15:42:47 +0100

    Initial app setup and configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/ui/header";
+import { SessionProvider } from "next-auth/react";
+import LandingPage from "./landing-page";
+
+export default function Home() {
+  return (
+    <SessionProvider>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center">
+        <LandingPage />
+      </main>
+    </SessionProvider>
+  );
+}
