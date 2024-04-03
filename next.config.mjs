09fc0ca707ce30c9148674de6e6a43c0a0fa6e0d
Ayo <Ayo.meshe@gmail.com>
2024-04-03 16:27:19 +0100

    app update v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
